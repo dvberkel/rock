d62f7ce16e4826b0a024d550589b291f480d20e6
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-08-14 12:34:52 +0200

    Tree structure
    A real tree structure is introduced.

--- lib/rock/bag.rb
+++ lib/rock/bag.rb
@@ -1,3 +1,5 @@
+require "set"
+
 class Bag
   def initialize()
     @pieces = {}
@@ -23,14 +25,32 @@ class MarkTree
   include Enumerable
 
   def initialize()
-    @pieces = []
+    @pieces = Set.new()
+    @sub_trees = {}
   end
 
   def add(piece)
-    @pieces << piece
+    Rotations.of(piece.sides).each do |trail|
+      branch_of(trail, piece)
+    end
+  end
+
+  def branch_of(trail, piece)
+    if (trail.empty?) then
+      @pieces << piece
+    else
+      head = trail[0]
+      if !(@sub_trees.has_key?(head)) then
+        @sub_trees[head] = MarkTree.new()
+      end
+      @sub_trees[head].branch_of(trail.slice(1,trail.size), piece)
+    end
   end
 
   def each
     @pieces.each {|piece| yield piece}
+    @sub_trees.each_value do |sub_tree|
+      sub_tree.each {|piece| yield piece}
+    end
   end
 end
